903cd4f39c35a6de47af92a51b3baa60033749c8
havilez <havilez@gmail.com>
2015-05-21 19:28:00 -0400

    latest changes

--- app/controllers/guestbook_controller.js
+++ app/controllers/guestbook_controller.js
@@ -4,6 +4,14 @@ myAppControllers.controller('GuestBookController', ['$scope',
   
 
       $scope.submit_data = function(){
+
+      var dataURL = "https://ha-hair-proj3.firebaseio.com/salonData";
+  
+        
+
+//var salonCustomersRef = $firebaseObject(new Firebase(dataURL + '/' + 'salonCustomers'));
+
+	  
         alert('submit successfull');
       };
 
